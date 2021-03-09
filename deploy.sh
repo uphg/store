90fbbcf5adb50b42ef446e389f5407d9c2d72273
ChenNing02 <2633565885@qq.com>
2021-03-09 15:28:56 +0800

    完善提交命令

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ set -e
 yarn docs:build
 cd docs/.vuepress/dist
 
-echo -e "\n# build =="
+echo -e "\n# build"
 git init
 git add -A
-echo -e "\n# commit build =="
+echo -e "\n# commit build"
 git commit -m 'deploy'
-echo -e "\n# push Github =="
+echo -e "\n# push Github"
 git push -f git@github.com:uphg/store.git master:gh-pages
-echo -e "\n# push Gitee =="
+echo -e "\n# push Gitee"
 git push -f git@gitee.com:uphg/store.git master:gh-pages
 echo -e "\n# Exit the build folder"
 cd -
--- once.sh
+++ once.sh
@@ -1,12 +1,12 @@
 # 第一次关联两个仓库
 git init
 git add .
-echo -e "Git commit"
+echo -e "# commit"
 git commit -m "first commit"
-echo -e "\Git remote origin (GitHub | Gitee)"
+echo -e "\n# remote origin (GitHub | Gitee)"
 git remote add github git@github.com:uphg/store.git
 git remote add gitee git@gitee.com:uphg/store.git
-echo -e"\nGit push Github"
+echo -e"\n# push Github"
 git push github master:master
-echo -e"\nGit push Gitee"
+echo -e"\n# push Gitee"
 git push gitee master:master
